4eec03134c6520b2f52425888bceb67545d84ca9
Codapop <george.dikun91@gmail.com>
2023-02-02 20:18:10 +0800

    Update comment input string.

--- generate.py
+++ generate.py
@@ -6,5 +6,7 @@ gpt2.load_gpt2(sess)
 # Print is here because loading the session writes a ton of stuff to the terminal
 print('========================\n')
 
+comment = '''[comment body here]'''
+
 # Length is in characters, temperature is how random (higher is more), prefix is what the comment starts with/replies to
-gpt2.generate(sess, length=200, temperature=0.7, prefix="[comment to reply to]")
+gpt2.generate(sess, temperature=0.7, prefix='{"comment": "' + comment + '", "reply": "')
--- train.py
+++ train.py
@@ -9,7 +9,7 @@ if not os.path.isdir(os.path.join("models", model_name)):
 	gpt2.download_gpt2(model_name=model_name)
 
 # Train model with dataset and save a checkpoint
-file_name = "dataset.txt"
+file_name = "training_data/dataset/dataset9.json"
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               file_name,
