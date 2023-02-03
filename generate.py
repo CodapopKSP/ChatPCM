3c7ccedf845971bb7b959cf6a915c1deb44d854d
Codapop <george.dikun91@gmail.com>
2023-02-03 22:07:27 +0800

    Add better formatting to generation. Create deep_based directory. Rename dataset directory.

--- generate.py
+++ generate.py
@@ -1,12 +1,51 @@
 import gpt_2_simple as gpt2
 
+inputComment = '''Just reverse the funny colors and have MSNBC instead of Fox News and it would be true for the Trump admin.'''
+numReplyCandidates = 3
+maxReplyLength = 300
+
+# Choose which model you are using
+checkpoint_dir = 'deep_based'
+run_name = 'Key_Value0_1_plus3'
+
+# String manipulation to allow for parsing of data
+delimiter = '"}'
+comment_with_parsers = '[{"' + inputComment + '":"'
+
+# Start gpt2
 sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess)
+gpt2.load_gpt2(sess,
+               checkpoint_dir = checkpoint_dir,
+               run_name = run_name)
 
-# Print is here because loading the session writes a ton of stuff to the terminal
-print('========================\n')
+# Generate a comment using gpt-2-simple
+def generateReply(temp, comment):
+    return gpt2.generate(sess,
+                  temperature=temp,
+                  checkpoint_dir = checkpoint_dir,
+                  run_name = run_name,
+                  length = maxReplyLength,
+                  nsamples = numReplyCandidates,
+                  return_as_list = True,
+                  prefix = comment)
 
-comment = '''[comment body here]'''
+# Generate a list of replies
+reply_candidates_raw = []
+reply_candidates_raw += generateReply(0.7, comment_with_parsers)
+reply_candidates_raw += generateReply(0.9, comment_with_parsers)
+reply_candidates = []
 
-# Length is in characters, temperature is how random (higher is more), prefix is what the comment starts with/replies to
-gpt2.generate(sess, temperature=0.7, prefix='{"comment": "' + comment + '", "reply": "')
+# Clean the replies and generate a list of pure reply text
+for reply in reply_candidates_raw:
+    try:
+        if delimiter in reply:
+            reply_parts = reply.split(delimiter)
+            reply = reply_parts[0].replace(comment_with_parsers, '')
+            reply_candidates.append(reply)
+    except:
+        pass
+
+# Print organized list
+for reply in reply_candidates:
+    print(f"Comment: {inputComment}")
+    print(f"Reply:   {reply}\n")
--- train.py
+++ train.py
@@ -4,17 +4,24 @@ import requests
 
 # Download or verify model
 model_name = "124M"
+checkpoint_dir = 'deep_based'
+run_name = 'Key_Value0_1_plus3'
+
 if not os.path.isdir(os.path.join("models", model_name)):
 	print(f"Downloading {model_name} model...")
 	gpt2.download_gpt2(model_name=model_name)
 
 # Train model with dataset and save a checkpoint
-file_name = "training_data/dataset/dataset9.json"
+file_name = "training_data/dataset_key_value/dataset3.json"
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               file_name,
               model_name=model_name,
-              steps=1000)   # steps is max number of training steps
+              steps=200,
+              checkpoint_dir = 'Deep Based',
+              run_name = 'Key_Value0')   # steps is max number of training steps
 
 # Generate final example text
-gpt2.generate(sess)
+gpt2.generate(sess,
+              checkpoint_dir = checkpoint_dir,
+              run_name = run_name)
